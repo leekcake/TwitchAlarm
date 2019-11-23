98916a3e1e189987df621e4b55c1e27cdaab3c8f
Blue Electric <leekcake@protonmail.com>
2019-11-23 19:48:17 +0900

    Add base platform support layer

---
+++ TwitchAlarmDesktop/Container/DesktopPlatformUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchAlarmShared.Worker;
+
+namespace TwitchAlarmDesktop.Container
+{
+    public class DesktopPlatformUtils : PlatformUtils
+    {
+        public override string GetConfigBasePath()
+        {
+            return "config";
+        }
+
+        public override byte[] ReadAllBytes(string path)
+        {
+            return File.ReadAllBytes(path);
+        }
+
+        public override void WriteAllBytes(string path, byte[] data)
+        {
+            File.WriteAllBytes(path, data);
+        }
+    }
+}
---
+++ TwitchAlarmShared/Worker/PlatformUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchAlarmShared.Worker
+{
+    public abstract class PlatformUtils
+    {
+        public static PlatformUtils Instance { get; set; }
+
+        public abstract void WriteAllBytes(string path, byte[] data);
+
+        public abstract byte[] ReadAllBytes(string path);
+
+        public abstract string GetConfigBasePath();
+    }
+}
