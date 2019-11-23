34987be956ad940c3ecabc95cd713fc908a039eb
Blue Electric <leekcake@protonmail.com>
2019-11-24 00:01:32 +0900

    Add TwitchID

---
+++ TwitchAlarmShared/Container/TwitchID.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchAlarmShared.Container
+{
+    public class TwitchID
+    {
+        public const string ID = "_FILL_YOUR_TWITCH_ID_";
+        public const string SECRET_ID = "FILL_IT";
+    }
+}
